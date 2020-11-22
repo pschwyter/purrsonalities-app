69ba17dca622f502acafdf64e844b03e53f1178e
Philippe Schwyter <philippe.schwyter@ada.support>
2020-11-21 21:38:52 -0500

    did stuff

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log("API_ROOT_URL", process.env.REACT_APP_API_ROOT_URL)
+  const r = fetch("http://localhost:4000").then(r => console.log(r));
   return (
     <div className="App">
       <header className="App-header">
